0e37f3b84844f750f73137e5637ba854d199d6f1
TheLost1 <laust13@hotmail.com>
2024-03-19 09:38:21 +0100

    added key in comment

--- P8-Data.R
+++ P8-Data.R
@@ -1,5 +1,7 @@
 library(jsonlite)
-API<-"https://api.polygon.io/v2/aggs/ticker/O:SPY221104C00374000/range/1/minute/2022-10-04/2022-11-03?adjusted=true&sort=asc&limit=120&apiKey=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk"
+library(roptions)
+#apiKey=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk
+API<-"https://api.polygon.io/v2/aggs/ticker/O:SPY221104C00374000/range/1/day/2022-10-04/2022-11-03?adjusted=true&sort=asc&limit=360&apiKey=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk"
 
 raw <- jsonlite::fromJSON(API)
 Data <- raw$results
