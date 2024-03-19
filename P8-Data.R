ee0e514bbd56e285cf1b38e623d4e805ad258e1a
chri961q <144891948+chri961q@users.noreply.github.com>
2024-03-19 12:02:09 +0100

    Update P8-Data.R

--- P8-Data.R
+++ P8-Data.R
@@ -1,8 +1,27 @@
 library(jsonlite)
-library(roptions)
-#apiKey=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk
-API<-"https://api.polygon.io/v2/aggs/ticker/O:SPY221104C00374000/range/1/day/2022-10-04/2022-11-03?adjusted=true&sort=asc&limit=360&apiKey=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk"
+APIstock <- "https://api.polygon.io/v2/aggs/ticker/O:SPY221104C00"
+APItid <- "/range/1/minute/2022-10-04/2022-11-03?adjusted=true&sort=asc&limit=50000&api"
+APIkey <- "Key=CBQouP6k8C92g2XWhofZB5PCGgxI6lOk"
+
+startprice <- 349
+
+API<- paste(APIstock,as.String(as.integer(startprice*1000)),APItid, APIkey, sep = "")
+raw <- jsonlite::fromJSON(API)
+
+vw <- raw$results$vw
+t <- raw$results$t
+
+df0 <- data.frame(vw,t)
+
+
+for (i in (startprice+1):352) {
+K <- i*1000
+API<- paste(APIstock,as.String(as.integer(K)),APItid, APIkey, sep = "") 
 
 raw <- jsonlite::fromJSON(API)
-Data <- raw$results
-plot(Data$vw)
+vw <- raw$results$vw
+t <- raw$results$t
+df <- data.frame(vw,t)
+
+df0 <- merge(df0, df, by = "t", all = TRUE)
+}
